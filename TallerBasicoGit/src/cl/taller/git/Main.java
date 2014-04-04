2244a06fca32f55ed6bf4df91c6cd3317790c1d1
carvaras <carvaras@alumnos.ubiobio.cl>
2014-04-04 11:37:29 -0300

    Update Main.java
    linea agregada por web

--- TallerBasicoGit/src/cl/taller/git/Main.java
+++ TallerBasicoGit/src/cl/taller/git/Main.java
@@ -14,6 +14,8 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println("Segundo commit");
+        System.out.println("Liena Agregada");
+        
         
     }
     
